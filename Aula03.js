3f1cbf5630caba0cff1d83db842770ad44686555
thiagogarcia2000 <158522653+thiagogarcia2000@users.noreply.github.com>
2024-04-11 18:11:02 -0300

    Add files via upload

---
+++ Aula03.js
@@ -0,0 +1,15 @@
+// const numero1 = prompt("Digite seu email:")
+// const numero2 = prompt("Digite sua senha:")
+
+// const email_correto = "joao@gmail.com"
+// const senha_correta = "123456"
+
+// if(numero1 === email_correto){
+//     if(numero2 === senha_correta){
+//         alert("Login concluido com sucesso")
+//     }else{
+//         alert("Senha errada")
+//     }
+// }else{
+//     alert("Email incorreto")
+// }
